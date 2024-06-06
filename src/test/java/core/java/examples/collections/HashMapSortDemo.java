4e2c3c4314a04a110f2c53cb0ad7e5feb6cd7595
Koteswararao Majji <krmajji@gmail.com>
2024-06-06 17:25:24 +0530

    map sort with comprator in the same class from java utill class

--- src/test/java/core/java/examples/collections/HashMapSortDemo.java
+++ src/test/java/core/java/examples/collections/HashMapSortDemo.java
@@ -4,14 +4,16 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.LinkedHashMap;
-import java.util.List;
 import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Set;
 
 public class HashMapSortDemo {
 
 
-	
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
@@ -21,25 +23,28 @@ public class HashMapSortDemo {
 		namesMap.put("Satish", 9777);
 		namesMap.put("Ram", 9999);
 		namesMap.put("Krishna", 5777);
-		
-		
+
+		Comparator<Map.Entry<String, Integer>> valueComprator = new Comparator<Map.Entry<String, Integer>>() {
+
+			@Override
+			public int compare(Entry<String, Integer> m1, Entry<String, Integer> m2) {
+				// TODO Auto-generated method stub
+				//return m1.getKey().compareTo(m1.getKey());
+				return m1.getValue().compareTo(m2.getValue());
+			}
+		};
+
+		ArrayList<Map.Entry<String, Integer>> namesList = new ArrayList<>(namesMap.entrySet());
+		//Set<String> nameSet = new HashSet<>(namesMap.keySet());
 
 		
-		 
-		  
-		  
-		ArrayList<String> namesList = new ArrayList<>(namesMap.keySet());
+		Collections.sort(namesList,valueComprator);
 		System.out.println(namesList);
-		Collections.sort(namesList);
-		System.out.println(namesList);
-
-		LinkedHashMap<String, Integer> namesMap1 = new LinkedHashMap<String,Integer>(); 
-
-		for (String name:namesList) {
-
-			namesMap1.put(name, namesMap.get(name));
-		}
-		System.out.println(namesMap1);
+		 for (Map.Entry<String, Integer> entry : namesList) {
+		      System.out.println(entry.getKey() + " : " + entry.getValue());
+		    }
+		
+		
 	}
 
 }

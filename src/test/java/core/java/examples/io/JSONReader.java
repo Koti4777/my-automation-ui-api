8a3c991001ad89908f461365fb6b290d0dbba45e
Koteswararao Majji <krmajji@gmail.com>
2024-07-11 16:00:22 +0530

    added json reader with path and get from JsonNode

--- src/test/java/core/java/examples/io/JSONReader.java
+++ src/test/java/core/java/examples/io/JSONReader.java
@@ -1,24 +1,68 @@
 package core.java.examples.io;
 
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
+import java.io.File;
 import java.io.IOException;
+import java.util.Iterator;
 
-import net.minidev.json.parser.JSONParser;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectReader;
 
 public class JSONReader {
-
-	public static void main(String[] args) {
-	String path = System.getProperty("user.dir")+"//resources//payloads//CreateOrder.json";
-	try(FileInputStream stream = new FileInputStream(path)){
-		
-		JSONParser parse = new JSONParser();
-	} catch (FileNotFoundException e) {
-		// TODO Auto-generated catch block
-		e.printStackTrace();
-	} catch (IOException e) {
-		// TODO Auto-generated catch block
-		e.printStackTrace();
+	public static void readEmployeeDetails() {
+		String path = System.getProperty("user.dir")+"//resources//payloads//Employee.json";
+		 ObjectMapper mapper = new ObjectMapper();
+	     try {
+	             // reading json input from the file and mapping to object
+	             File jsonInputFile = new File(path);
+	             
+	             JsonNode rootNode = mapper.readTree(jsonInputFile);
+	             // read employee id
+	             JsonNode empId = rootNode.path("emp_id");
+	             System.out.println(empId.intValue());
+	             // read employee name
+	             JsonNode empName = rootNode.path("emp_name");
+	             System.out.println(empName.textValue());
+	             // read direct reports
+	             JsonNode drNode = rootNode.path("direct_reports");
+	           //  Iterator<JsonNode> itr = drNode.elements();
+	             Iterator<JsonNode> itr = rootNode.get("direct_reports").elements();
+	             System.out.println("\nDirect reports:");
+	             while (itr.hasNext()) {
+	                     JsonNode temp = itr.next();
+	                     System.out.println(temp.textValue());
+	             }
+	             
+	     } catch (IOException e) {
+	             // TODO Auto-generated catch block
+	             e.printStackTrace();
+	     }
 	}
+	public static void readOrderJOSN() {
+		String path = System.getProperty("user.dir")+"//resources//payloads//CreateOrder.json";
+		 ObjectMapper mapper = new ObjectMapper();
+	     try {
+	             // reading json input from the file and mapping to object
+	             File inputfile = new File(path);
+	             
+	             JsonNode rootNode = mapper.readTree(inputfile);
+	           
+	            // System.out.println(node.toString());
+	             // read tool ID
+	            String toolId = rootNode.get("toolId").asText();
+	            String customerName = rootNode.get("customerName").asText();
+	            String comment = rootNode.get("comment").asText();
+	             System.out.println("\"toolId\":"+toolId);  
+	             System.out.println("\"customerName\":"+customerName); 
+	             System.out.println("\"comment\":"+comment); 
+	     } catch (IOException e) {
+	             // TODO Auto-generated catch block
+	             e.printStackTrace();
+	     }
+	}
+	public static void main(String[] args) {
+		readEmployeeDetails();
+		System.out.println("Create Order details"+"\n");
+		readOrderJOSN();
 	}
 }

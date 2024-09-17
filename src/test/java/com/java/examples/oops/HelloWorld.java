c607b3a707711f152d8e8d5fcb39d72d5b96ef65
Koteswararao Majji <koteswararao.majji@volantetech.com>
2024-09-17 14:48:04 +0530

    new download changes

---
+++ src/test/java/com/java/examples/oops/HelloWorld.java
@@ -0,0 +1,12 @@
+package com.java.examples.oops;
+
+import io.opentelemetry.exporter.logging.SystemOutLogRecordExporter;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("HelloWorld");
+	}
+
+}
--- src/test/java/com/mycompany/automation/AppTest.java
+++ src/test/java/com/mycompany/automation/AppTest.java
@@ -1,4 +1,5 @@
 package com.mycompany.automation;
+
 import org.openqa.selenium.WebDriver;
 import org.testng.ITestResult;
 import org.testng.annotations.AfterTest;
@@ -12,42 +13,41 @@ import com.mycompany.utils.Driver;
  * Unit test for simple App.
  */
 public class AppTest {
-WebDriver driver;
-ExtentReports report;
-public AppTest() {
-	this.driver=Driver.getInstance();
-}
+	/*
+	 * WebDriver driver; ExtentReports report;
+	 * 
+	 * public AppTest() { this.driver = Driver.getInstance(); }
+	 */
 
-	@Test
-	public void shouldAnswerWithTrue() {
+	/*
+	 * @Test public void shouldAnswerWithTrue() {
+	 * 
+	 * driver.get("https://www.google.com");
+	 * SeleniumTestHelper.assertEquals("Hello", "Hello", null); }
+	 * 
+	 * @AfterTest
+	 * 
+	 * public void report(ITestResult result) { ExtentReports report = new
+	 * ExtentReports(); ExtentHtmlReporter htmlReport = new
+	 * ExtentHtmlReporter(System.getProperty("user.dir")+
+	 * "\\extentreports\\"+this.getClass()+".html");
+	 * report.attachReporter(htmlReport); report.getClass();
+	 * 
+	 * }
+	 * 
+	 * @Test public void sampleText() {
+	 * 
+	 * ChromeOptions chromeOptions = new ChromeOptions();
+	 * WebDriverManager.chromedriver().setup(); WebDriver driver = new
+	 * ChromeDriver(chromeOptions); driver.manage().window().maximize();
+	 * 
+	 * String methodName = new Object(){}.getClass().getEnclosingMethod().getName();
+	 * driver.get("https://demoqa.com/elements");
+	 * SeleniumTestHelper.getScreenshot(methodName);
+	 * 
+	 * System.out.println("Method name is "+methodName);
+	 * 
+	 * }
+	 */
 
-		driver.get("https://www.google.com");
-		SeleniumTestHelper.assertEquals("Hello", "Hello", null);
-	}
-
-	@AfterTest
-	 
-	 public void report(ITestResult result) {
-		ExtentReports report = new ExtentReports();
-		ExtentHtmlReporter htmlReport = new ExtentHtmlReporter(System.getProperty("user.dir")+"\\extentreports\\"+this.getClass()+".html");
-		report.attachReporter(htmlReport);
-		report.getClass();
-		
-	}
-	@Test
-	public void sampleText() {
-		/*
-		 * ChromeOptions chromeOptions = new ChromeOptions();
-		 * WebDriverManager.chromedriver().setup(); WebDriver driver = new
-		 * ChromeDriver(chromeOptions); driver.manage().window().maximize();
-		 */
-		String methodName = new Object(){}.getClass().getEnclosingMethod().getName();
-		driver.get("https://demoqa.com/elements");
-		SeleniumTestHelper.getScreenshot(methodName);
-	
-		System.out.println("Method name is "+methodName);
-		
-	}
-
-	
 }
